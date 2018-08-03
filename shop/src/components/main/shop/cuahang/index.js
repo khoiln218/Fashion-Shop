a379ee2ddbf06322938b950b462aa81bf1148033
Khoi Le <khoiln218@gmail.com>
2018-08-03 22:37:00 +0700

    done phase 1

--- shop/src/components/main/shop/cuahang/index.js
+++ shop/src/components/main/shop/cuahang/index.js
@@ -3,23 +3,122 @@
 import React, { Component } from 'react';
 import {
   View,
-  Text,
   StyleSheet,
+  TextInput,
+  Image,
+  Dimensions,
+  FlatList,
+  TouchableOpacity,
+  Text
 } from 'react-native';
 
+import icSearch from '../../../../media/appIcon/search.png';
+import icAddress from '../../../../media/appIcon/map-marker.png';
+
+const data = [
+  { id: '1', name: 'Nguyễn Kim Trần Hưng Đạo', address: '60-61-62 Trần Hưng Đạo, Q1' },
+  { id: '2', name: 'Điện Máy Xanh CMT8', address: '60-61-62 CMT8, Q3' },
+  { id: '3', name: 'Điện Máy Chợ Lớn CMT8', address: '158 CMT8, Q3' },
+  { id: '4', name: 'Điện Máy Xanh CMT8', address: '185 CMT8, Q3' },
+  { id: '5', name: 'Điện Máy Chợ Lớn CMT8', address: '158 CMT8, Q3' },
+  { id: '6', name: 'Điện Máy Xanh CMT8', address: '185 CMT8, Q3' },
+  { id: '7', name: 'Nguyễn Kim Trần Hưng Đạo', address: '60-61-62 Trần Hưng Đạo, Q1' },
+  { id: '8', name: 'Điện Máy Xanh CMT8', address: '60-61-62 CMT8, Q3' },
+  { id: '9', name: 'Điện Máy Xanh CMT8', address: '185 CMT8, Q3' },
+  { id: '10', name: 'Nguyễn Kim Trần Hưng Đạo', address: '60-61-62 Trần Hưng Đạo, Q1' },
+  { id: '11', name: 'Điện Máy Xanh CMT8', address: '185 CMT8, Q3' },
+  { id: '12', name: 'Điện Máy Xanh CMT8', address: '60-61-62 CMT8, Q3' },
+];
+
 export default class CuaHang extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      key: ''
+    };
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>I'm the CuaHang component</Text>
+        <View style={styles.searchContainer}>
+          <View style={styles.searchRound}>
+            <TextInput
+              style={styles.searchTxt}
+              onChangeText={(key) => this.setState({ key })}
+              placeholder="Tìm kiếm cửa hàng..."
+              underlineColorAndroid="transparent"
+              value={this.state.key}
+            />
+            <Image source={icSearch} style={styles.icon} />
+          </View>
+        </View>
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View>
+              <TouchableOpacity style={styles.wrapper}>
+                <View style={styles.row1}>
+                  <Image source={icAddress} style={styles.icon2} />
+                  <Text>{item.name}</Text>
+                </View>
+                <Text style={{ marginLeft: 20 }}>{item.address}</Text>
+              </TouchableOpacity>
+              <View style={styles.sepator} />
+            </View>
+          )}
+        />
       </View>
     );
   }
 }
 
+const { height, width } = Dimensions.get('window');
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E54279'
+    backgroundColor: '#FFFFFF'
   },
+  searchContainer: {
+    height: height / 15,
+    backgroundColor: '#444444'
+  },
+  searchRound: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    backgroundColor: '#DEDEDE',
+    borderColor: 'white',
+    borderRadius: 20,
+    margin: 7
+  },
+  searchTxt: {
+    width: width - 60,
+    fontSize: 12,
+    padding: 0
+  },
+  icon: {
+    width: 20,
+    height: 20,
+    marginLeft: 10
+  },
+  row1: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 5
+  },
+  icon2: {
+    width: 15,
+    height: 15,
+    marginRight: 5
+  },
+  sepator: {
+    height: 1,
+    backgroundColor: '#E8E8E8'
+  },
+  wrapper: {
+    paddingVertical: 15,
+    paddingHorizontal: 15
+  }
 });
--- shop/src/components/main/shop/sanphamkhac/index.js
+++ shop/src/components/main/shop/sanphamkhac/index.js
@@ -5,13 +5,38 @@ import {
   View,
   Text,
   StyleSheet,
+  TouchableOpacity,
+  Image
 } from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import icArrow from '../../../../media/appIcon/chevron-right.png';
 
 export default class SanPhamKhac extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>I'm the SanPhamKhac component</Text>
+        <TouchableOpacity>
+          <View style={styles.wrapper}>
+            <View style={styles.left}>
+              <Icon style={{ margin: 12 }} name='camera' size={30} color='#000000' />
+              <Text style={{ fontSize: 18 }}>UPLOAD HÌNH ẢNH QUẦY HÀNG</Text>
+            </View>
+            <Image source={icArrow} style={{ width: 48, height: 48 }} />
+          </View>
+          <View style={{ height: 1, backgroundColor: '#F1F1F1' }} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={styles.wrapper}>
+            <View style={styles.left}>
+              <Icon style={{ margin: 12 }} name='dropbox' size={30} color='#000000' />
+              <Text style={{ fontSize: 18 }}>THÔNG TIN SẢN PHẨM KHÁC</Text>
+            </View>
+            <Image source={icArrow} style={{ width: 48, height: 48 }} />
+          </View>
+          <View style={{ height: 1, backgroundColor: '#F1F1F1' }} />
+        </TouchableOpacity>
       </View>
     );
   }
@@ -20,6 +45,16 @@ export default class SanPhamKhac extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#423939'
+    backgroundColor: '#FFFFFF'
   },
+  wrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 15
+  },
+  left: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  }
 });
